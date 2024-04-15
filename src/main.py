548e338f917ee12e230744c6cfcc1f2ecb04b587
Juan Avendaño <juanandreslujan30@gmail.com>
2024-04-15 06:33:55 -0500

    Arreglado problemas con el servidor

--- src/main.py
+++ src/main.py
@@ -7,19 +7,26 @@ CORS(app)  # Aplica CORS a toda la aplicación
 
 @app.route('/lexer', methods=['POST'])
 def main(): 
-    """
+   
     #lee el contendio de test.lang y lo guarda en una variable
+    """
     content = ""
     with open('src/test.lang', 'r') as file:
         content = file.read()
-    """
+   """
+    
     content = request.json['content']
-    #lexer
+    
+   #lexer
     #Llamamos la clase lexer y lo inicializamos con el codigo fuente
     lex = lexer.lexer(content)
     #Ahora  llamamos al metodo "tokenize"
     tokens = lex.tokenize()
-    return jsonify(tokens)
+    return jsonify({'tokens': tokens})
+    #return tokens
+
+#main()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
