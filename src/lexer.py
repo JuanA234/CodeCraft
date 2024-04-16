b356dc8d290a8d316f25aa4e22bcf6d8f87ff037
Juan Avendaño <juanandreslujan30@gmail.com>
2024-04-16 18:55:46 -0500

    Agregando lista de duplas en vez de diccionario

--- src/lexer.py
+++ src/lexer.py
@@ -9,72 +9,73 @@ class Lexer(object):
         self.source_code = source_code
     
     def tokenize(self):
-        # Aquí vamos a guardar los tokens creados por el lexer
-        tokens = {}
+        #Aquí vamos a guardar los tokens creados por el lexer
+        tokens = []
 
-        # Creamos una lista de palabras del codigo fuente
+        #Creamos una lista de palabras del codigo fuente
         source_code = self.source_code.split()
 
-        # Esto seguira la pista del indice de la palabra en el "source_code"
+        #Esto seguira la pista del indice de la palabra en el "source_code"
         source_index = 0
 
-        # Iteramos a traves de cada palabra en el "source_code" para generar los tokens
+        #Iteramos a traves de cada palabra en el "source_code" para generar los tokens
         while source_index < len(source_code):
 
             word = source_code[source_index]
 
-            # Esto va a reconocer una variable y crear un token para ella
+            #Esto va a reconocer una variable y crear un token para ella
             if word == "bloque": 
-                self.add_to_dict(tokens, "DeclaracionEntero", word)
+                tokens.append(["DeclaracionEntero", word])
             
             elif word == "hiloRedstone":
-                self.add_to_dict(tokens, "DeclaracionString", word)
+                tokens.append(["DeclaracionString", word])
             
             elif word == "pasoHelado":
-                self.add_to_dict(tokens, "DeclaracionFloat", word)
+                 tokens.append(["DeclaracionFloat", word])
                 
             elif word == "booleanman":
-                self.add_to_dict(tokens, "DeclaracionBoolean", word)
+                tokens.append(["DeclaracionBoolean", word])
             
             elif word in conditionals:
-                self.add_to_dict(tokens, "Condicional", word)
+                tokens.append(["Condicional", word])
             
             elif word in logicOperators:
-                self.add_to_dict(tokens, "OperadorLogico", word)
+                tokens.append(["OperadorLogico", word])
             
             elif word == "minarPara":
-                self.add_to_dict(tokens, "CicloFor", word)
+                tokens.append(["CicloFor", word])
             
             elif word == "mientrasNoDiamante":
-                self.add_to_dict(tokens, "CicloWhile", word)
+                tokens.append(["CicloWhile", word])
             
             elif word == "picoRoto":
-                self.add_to_dict(tokens, "palabraReservadaPicoRoto", word)
+                tokens.append(["palabraReservadaPicoRoto", word])
             
             elif word == "seguirPicando":
-                self.add_to_dict(tokens, "palabraReservadaPicoRotoSeguitPicando", word)
+                tokens.append(["palabraReservadaPicoRotoSeguitPicando", word])
     
-            # Reconocera una palabra y creara un indentificador para esta misma
+            #Reconocera una palabra y creara un indentificador para esta misma
             elif re.match('[a-z]', word) or re.match('[A-Z]', word):
                 if word[len(word)-1] == ";":
-                    self.add_to_dict(tokens, "Identificador", word[0:len(word)-1])
+                    tokens.append(['Identificador', word[0:len(word)-1]])
                 else:
-                    self.add_to_dict(tokens, "Identificador", word)  # aclarar
+                    tokens.append(['Identificador', word]) #aclarar
             
-            # Reconocera un entero y creara un indentificador para este mismo
+            
+            #Reconocera un entero y creara un indentificador para este mismo
             elif re.match('[0-9]', word):
                 if word[len(word)-1] == ";":
-                    self.add_to_dict(tokens, "Entero", word[0:len(word)-1])
+                    tokens.append(['Entero', word[0:len(word)-1]])
                 else:
-                    self.add_to_dict(tokens, "Entero", word)  # aclarar
+                    tokens.append(['Entero', word]) #aclarar
             
-            # Reconocera un operador y creara un indentificador para este mismo
+            ##Reconocera un operador y creara un indentificador para este mismo
             elif word in "=/*=-+":
-                self.add_to_dict(tokens, "Operador", word)
+                tokens.append(['Operador', word])
             
-            # Si un ; se encuentra al final de una palabra se agrega un token FinSentencia
+            #Si un ; se encuentra al final de una palabra se agrega un token FinSentencia
             if word[len(word)-1] == ";":
-                self.add_to_dict(tokens, "FinSentencia", ";")
+                tokens.append(['FinSentencia', ";"])
 
             source_index += 1
 
@@ -82,8 +83,3 @@ class Lexer(object):
 
         return tokens
 
-    def add_to_dict(self, dictionary, key, value):
-        if key in dictionary:
-            dictionary[key].append(value)
-        else:
-            dictionary[key] = [value]
